13f0b24ab6302b023a75fb9820dfe1e6a21cc1f9
kreespyn <santiago.crispin.villegas@gmail.com>
2023-07-25 21:10:35 -0500

    Este sí fue

---
+++ main.py
@@ -0,0 +1,61 @@
+from typing import List
+from fastapi import FastAPI, Depends
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.ext.declarative import declarative_base
+from pydantic import BaseModel
+
+DATABASE_URL = "postgresql://kreespyn:12345@localhost:5432/mydatabase"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+class Location(Base):
+    __tablename__ = "locations"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    country = Column(String)
+    image = Column(String)
+    description = Column(String)
+
+Base.metadata.create_all(bind=engine)
+
+class LocationBase(BaseModel):
+    name: str
+    country: str
+    image: str
+    description: str
+
+class LocationCreate(LocationBase):
+    pass
+
+class LocationModel(LocationBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+app = FastAPI()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.post("/locations/", response_model=LocationModel)
+def create_location(location: LocationCreate, db: Session = Depends(get_db)):
+    db_location = Location(**location.dict())
+    db.add(db_location)
+    db.commit()
+    db.refresh(db_location)
+    return db_location
+
+@app.get("/locations/", response_model=List[LocationModel])
+def read_locations(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    locations = db.query(Location).offset(skip).limit(limit).all()
+    return locations
